4506cb5e6565bff4f27ca5302504c9516b5fa140
Павел Кононенко <storuky@gmail.com>
2015-07-27 23:30:53 +0300

    v2

--- app/assets/javascripts/directives/dimension_directive.js
+++ app/assets/javascripts/directives/dimension_directive.js
@@ -7,13 +7,14 @@ app.directive('afDimension', ['$rootScope', function($rootScope) {
     scope: {
       ngModel: "=ngModel",
       options: "=options",
+      before: "=before"
     }, // {} = isolate, true = child, false/undefined = no change
     // controller: function($scope, $element, $attrs, $transclude) {},
     // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
     // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
     // template: '',
     templateUrl: 'dimension.html',
-    // replace: true,
+    replace: true,
     // transclude: true,
     // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
     link: function($scope, iElm, iAttrs, controller) {
