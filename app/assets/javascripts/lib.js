d22d43179604a55dc4791c8fe8fc60e6e3a8c7d4
Павел Кононенко <storuky@gmail.com>
2015-08-02 15:14:02 +0300

    ripple and widgets anim

--- app/assets/javascripts/lib.js
+++ app/assets/javascripts/lib.js
@@ -37,3 +37,11 @@ window.guid = function () {
 window.htmlToPlaintext = function (text) {
   return String(text).replace(/<[^>]+>/gm, '');
 }
+
+window.getElementOffset = function (element) {
+  var de = document.documentElement;
+  var box = element.getBoundingClientRect();
+  var top = box.top + window.pageYOffset - de.clientTop;
+  var left = box.left + window.pageXOffset - de.clientLeft;
+  return { top: top, left: left };
+}
