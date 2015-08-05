f8190f387dd79e7984ed3fe28bcf381fcae89d19
Павел Кононенко <storuky@gmail.com>
2015-08-05 23:43:23 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'stylus'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,9 +11,8 @@ module Wheat
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    Stylus.debug = false
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
--- config/initializers/stylus.rb
+++ config/initializers/stylus.rb
@@ -1,1 +1,2 @@
-# 
+require 'stylus'
+Stylus.debug = false
